fddaab98195713db596fc135ee566dbd0a116796
Marcin Wilk <marcin@marcinwilk.eu>
2021-03-13 18:26:07 +0100

    Change yellow to orange color
    Changed username color so it looks better on bright backgrounds.

--- MOTD-EL/10-banner.sh
+++ MOTD-EL/10-banner.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 user="$(whoami)"
-echo '- -- -- ------ Audaces Fortuna Iuvat  ------ -- -- -' | lolcat -f
-echo -e "Welcome \e[93m$user \e[39mat:"
+echo "- -- -- ------ Audaces Fortuna Iuvat  ------ -- -- -" | lolcat -f
+echo -e "Welcome \e[38;5;214m$user \e[39;0mat:"
